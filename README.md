8122900f6e936286af3059aa59d3560d34881c83
Qiang Shen <johnsonzhj123@gmail.com>
2017-10-30 10:14:21 +0800

    course 5

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ Welcome to the class for python and data analysis! Hope we can enjoy a great per
 The .ipynb files for the course will be uploaded ahead of the class
 
 - Class Time: Monday 8-9 (3:25-5:05 pm)
-- Class Venue: Room 502, Boyi Building B
+- Class Venue: Room 402, Boyi Building B
---
+++ course 5/assignment.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 29 23:26:44 2017
+
+@author: qiangshen
+"""
+
+
+##slicing return view of the original array
+
+from IPython.core.interactiveshell import InteractiveShell
+InteractiveShell.ast_node_interactivity = "all"##last
+
+
+import numpy as np
+x=np.reshape(np.arange(4.0),(2,2))
+
+s1=x[0,:]
+s2=x[:,0]
+s1;s2
+
+
+s1[0]=-3.14
+
+
+%reset -f
+
+import numpy as np
+x=np.reshape(np.arange(4.0),(2,2))
+s1=np.copy(x[0,:]) ##function copy
+s2=x[:,0].copy() ## method copy,more common
+s3=np.array(x[0,:]) ## create a new array
+
+s1[0]=-3.14
+
+
+
+
+###Scalar
+%reset -f
+
+import numpy as np
+x=np.arange(5.0)
+
+y=x[0]
+
+z=x[:1]
+
+y=-3.14
+
+
+z[0]=-2.13
+
+
+
+### functions
+%reset -f
+import numpy as np
+x=np.array([[0.0,1],[2,3]])
+
+y=x
+
+id(x);id(y)
+
+y= x+1.2
